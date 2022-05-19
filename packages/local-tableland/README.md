050b43692aecf594baa9834d89042cf570643b57
Joe Wagner <njwjs722@gmail.com>
2022-05-19 10:01:47 -0600

    Initial commit

---
+++ packages/local-tableland/README.md
@@ -0,0 +1,24 @@
+# End to End Local Tableland script
+
+This is a hacked together Deno script that will get all aspect of Tableland running locally.
+This can be useful to test deploying Policy Contracts
+
+## Running
+
+you'll need a the paths to the go-tableland and the eth-tableland repositories to be available as env variables, you'll need docker running, and you need to run the script with some flags. Using `--allow-run` enables subprocesses, and `--allow-env` enables the script to read the env vars
+
+Example:
+
+```
+export HARDHAT_DIR=<your path to the eth-tableland repo>
+export VALIDATOR_DIR=<your path to the go-tableland repo>/local # <- notice the '/local' !!
+deno run --allow-run --allow-env e2e-test.js
+```
+
+Everything should be running and you are now free to develop your tableland app by connecting to the local validator at http://localhost:8080 and/or the local blockchain at http://localhost:8545
+
+## Notes
+
+ - Make sure to have Docker running before doing this.
+ - Keep and eye out for Zombie processes. Killing the Deno process should kill all of the subprocesses, but this kind of script is prone to leaking zombies.
+ - There are default values for the registry contract address and the validator wallet address that should match what is in eth-tableland, but you can edit these in `go-tableland/local/config.json`
---
+++ packages/local-tableland/e2e-test.js
@@ -0,0 +1,120 @@
+/**
+ * Run end to end Tableland
+ */
+import { readLines } from 'https://deno.land/std@0.140.0/io/mod.ts';
+import { writeAll } from 'https://deno.land/std@0.140.0/io/util.ts';
+import { cyan, brightGreen, magenta, red } from 'https://deno.land/std@0.140.0/fmt/colors.ts'
+
+const cleanup = async function () {
+    const pruneContainer = Deno.run({cmd: [
+        'docker',
+        'container',
+        'prune',
+        '-f'
+    ]})
+    await pruneContainer.status();
+
+    const removeApi = Deno.run({cmd: [
+        'docker',
+        'image',
+        'rm',
+        'local_api',
+        '-f'
+    ]})
+    await removeApi.status();
+
+    const removeDb = Deno.run({cmd: [
+        'docker',
+        'image',
+        'rm',
+        'local_database',
+        '-f'
+    ]})
+    await removeDb.status();
+
+    const pruneVolume = Deno.run({cmd: [
+        'docker',
+        'volume',
+        'prune',
+        '-f'
+    ]})
+    await pruneVolume.status()
+
+};
+
+const pipeNamedSubprocess = async function (prefix, reader, writer) {
+  const encoder = new TextEncoder();
+  for await (const line of readLines(reader)) {
+    await writeAll(writer, encoder.encode(`[${prefix}] ${line}\n`));
+  }
+}
+
+const wait = async function (ms) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(() => resolve(), ms);
+    });
+}
+
+
+const start = async function () {
+    // make sure we are starting fresh
+    await cleanup();
+
+    // Run a local hardhat node
+    const hardhat = Deno.run({
+        cwd: Deno.env.get('HARDHAT_DIR'),
+        cmd: [
+            'npm',
+            'run',
+            'up'
+        ],
+        stdout: 'piped',
+        stderr: 'piped'
+    });
+    console.log(hardhat);
+    // NOTE: the process should keep running until we kill it
+    pipeNamedSubprocess(cyan('Hardhat'), hardhat.stdout, Deno.stdout);
+    pipeNamedSubprocess(red('Hardhat'), hardhat.stderr, Deno.stderr);
+
+    // very naive way to let the Hardhat node start before deploying to it
+    await wait(21 * 1000);
+
+    // Deploy the Registry to the Hardhat node
+    const deployRegistry = Deno.run({
+        cwd: Deno.env.get('HARDHAT_DIR'),
+        cmd: [
+            'npx',
+            'hardhat',
+            'run',
+            '--network',
+            'localhost',
+            'scripts/deploy.ts'
+        ],
+        stdout: 'piped',
+        stderr: 'piped'
+    });
+    pipeNamedSubprocess(brightGreen('Deploy Registry:'), deployRegistry.stdout, Deno.stdout);
+    pipeNamedSubprocess(red('Deploy Registry:'), deployRegistry.stderr, Deno.stderr);
+
+    // wait till the deploy finishes
+    await deployRegistry.status();
+
+    // start the validator
+    const validator = Deno.run({
+        cwd: Deno.env.get('VALIDATOR_DIR'),
+        cmd: [
+            'make',
+            'up'
+        ],
+        env: {
+            CONFIG_FILE: 'config.json'
+        },
+        stdout: 'piped',
+        stderr: 'piped'
+    });
+    // NOTE: the process should keep running until we kill it
+    pipeNamedSubprocess(magenta('Validator'), validator.stdout, Deno.stdout);
+    pipeNamedSubprocess(red('Validator'), validator.stderr, Deno.stderr);
+}
+
+await start();
