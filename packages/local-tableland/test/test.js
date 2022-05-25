7ec977f0ba640ce2bf97eebe9dd482b19b40840b
Joe Wagner <njwjs722@gmail.com>
2022-05-25 10:30:18 -0600

    Add test for unauthorized call to runSQL

--- packages/local-tableland/test/test.js
+++ packages/local-tableland/test/test.js
@@ -5,12 +5,12 @@ import { createInterface } from 'readline';
 import { jest } from '@jest/globals';
 
 // These tests take a bit longer than normal since we are usually waiting for blocks to finalize etc...
-jest.setTimeout(12000);
+jest.setTimeout(20000);
 
 const getTableId = async function (tableland, txnHash) {
     let table = await tableland.receipt(txnHash);
     let tries = 0
-    while (!table && tries < 3) {
+    while (!table && tries < 5) {
         await new Promise(resolve => setTimeout(resolve, 1500 + (tries * 500)));
         table = await tableland.receipt(txnHash);
         tries++;
@@ -46,6 +46,46 @@ describe("Validator, Chain, and SDK work end to end", function () {
         await expect(data.rows).toEqual([]);
     });
 
+    test("Table cannot be written to unless caller is allowed", async function () {
+        const wallet = new Wallet('0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d' /* Hardhat #1 */);
+        const provider = new providers.JsonRpcProvider('http://localhost:8545');
+        const signer = wallet.connect(provider);
+
+        const tableland = await connect({
+            signer: signer,
+            network: 'local',
+            host: 'http://localhost:8080'
+        });
+
+        const prefix = 'test_not_allowed';
+        const receipt = await tableland.create(31337, 'key TEXT, val TEXT', prefix);
+
+        const tableId = await getTableId(tableland, receipt.transactionHash);
+        const chainId = 31337;
+        const queryableName = `${prefix}_${chainId}_${tableId}`;
+
+        const data = await tableland.read(`SELECT * FROM ${queryableName};`);
+        await expect(data.rows).toEqual([]);
+
+        const wallet2 = new Wallet('0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a' /* Hardhat #2 */);
+        const provider2 = new providers.JsonRpcProvider('http://localhost:8545');
+        const signer2 = wallet2.connect(provider2);
+
+        const tableland2 = await connect({
+            signer: signer2,
+            network: 'local',
+            host: 'http://localhost:8080'
+        });
+
+        const writeRes = await tableland2.write(`INSERT INTO ${queryableName} (key, val) VALUES ('tree', 'aspen')`);
+
+        await new Promise(resolve => setTimeout(resolve, 4000));
+
+        const data2 = await tableland2.read(`SELECT * FROM ${queryableName};`);
+        console.log(data);
+        await expect(data.rows).toEqual([]);
+    });
+
     test("Create a table that can be written to", async function () {
         const wallet = new Wallet('0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d' /* Hardhat #1 */);
         const provider = new providers.JsonRpcProvider('http://localhost:8545');
@@ -69,7 +109,6 @@ describe("Validator, Chain, and SDK work end to end", function () {
         await new Promise(resolve => setTimeout(resolve, 4000));
 
         const data = await tableland.read(`SELECT * FROM ${queryableName};`);
-        console.log(data);
         await expect(data.rows).toEqual([['tree', 'aspen']]);
     });
 
@@ -94,14 +133,14 @@ describe("Validator, Chain, and SDK work end to end", function () {
         await tableland.write(`INSERT INTO ${queryableName} (key, val) VALUES ('tree', 'aspen')`);
         await tableland.write(`INSERT INTO ${queryableName} (key, val) VALUES ('tree', 'pine')`);
 
-        await new Promise(resolve => setTimeout(resolve, 3000));
+        await new Promise(resolve => setTimeout(resolve, 6000));
 
         const data = await tableland.read(`SELECT * FROM ${queryableName};`);
         await expect(data.rows.length).toEqual(2);
 
         await tableland.write(`DELETE FROM ${queryableName} WHERE val = 'pine';`);
 
-        await new Promise(resolve => setTimeout(resolve, 3000));
+        await new Promise(resolve => setTimeout(resolve, 4000));
 
         const data2 = await tableland.read(`SELECT * FROM ${queryableName};`);
         await expect(data2.rows.length).toEqual(1);
