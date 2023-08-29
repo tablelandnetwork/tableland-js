5473f9138b371b0914ef853644d9cdc07b2be9f6
Joe Wagner <njwjs722@gmail.com>
2023-08-29 13:19:14 -0600

    (local) fix solidity version in registry contract

--- packages/local/registry/contracts/TablelandTables.sol
+++ packages/local/registry/contracts/TablelandTables.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity 0.8.19;
 
 // Need to have this so the tests can deploy a registry
 import "@tableland/evm/contracts/TablelandTables.sol";
